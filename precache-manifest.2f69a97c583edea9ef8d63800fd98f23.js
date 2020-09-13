061380a6d956ca9ddc5816ba8d81b74ae1cd1217
Juan Ro <juanro.pf@gmail.com>
2020-09-12 20:05:04 -0500

    Updates

---
+++ precache-manifest.2f69a97c583edea9ef8d63800fd98f23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3133e4f3c8934e3aac6910db2e99d0fb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "66600c7f0b3d8322d7dd",
+    "url": "/portfolio/static/css/main.38a8e065.chunk.css"
+  },
+  {
+    "revision": "7233bb4ac04ae6fbbf09",
+    "url": "/portfolio/static/js/2.284fab75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.284fab75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66600c7f0b3d8322d7dd",
+    "url": "/portfolio/static/js/main.be95c347.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4c593bbef1e066fd65f9fec366a37065",
+    "url": "/portfolio/static/media/Brazo.4c593bbe.png"
+  },
+  {
+    "revision": "7d7ec934906a53e7348c976c3dfde812",
+    "url": "/portfolio/static/media/CanadaImage.7d7ec934.png"
+  },
+  {
+    "revision": "d3c1ed91f40fcd324a68a0b8a56c9215",
+    "url": "/portfolio/static/media/Cluster.d3c1ed91.png"
+  },
+  {
+    "revision": "87904d9494e0bdd4baf75fa407671cef",
+    "url": "/portfolio/static/media/ResumeJR.87904d94.docx"
+  },
+  {
+    "revision": "d074cda20e2b29d649429fc5a555d839",
+    "url": "/portfolio/static/media/Titulo.d074cda2.JPG"
+  }
+]);
