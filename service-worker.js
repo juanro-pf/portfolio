f5d20471afb3b0e933c7097c77f534a63055c0a2
JuanRo <juanro.pf@gmail.com>
2020-09-26 13:36:01 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.07f767f6d7b32adcbd8beff12bac16ca.js"
+  "/portfolio/precache-manifest.ec08dcf1768df5b35746eb9129ce4a6c.js"
 );
 
 self.addEventListener('message', (event) => {
