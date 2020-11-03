b3be90f2185449b504df014579d8f15e3af8a18b
Juan Ro <juanro.pf@gmail.com>
2020-11-02 18:58:31 -0600

    Updates

---
+++ precache-manifest.eee901f261689dee78c498c8fbfff994.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f468bbd800ad2dc6cfeada07aaaac8d4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "66426390823f252484c1",
+    "url": "/portfolio/static/css/main.08e05271.chunk.css"
+  },
+  {
+    "revision": "0caea02e6b9ff994cbfe",
+    "url": "/portfolio/static/js/2.45fb2816.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.45fb2816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66426390823f252484c1",
+    "url": "/portfolio/static/js/main.92c76750.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4c593bbef1e066fd65f9fec366a37065",
+    "url": "/portfolio/static/media/Brazo.4c593bbe.png"
+  },
+  {
+    "revision": "e9f1b88c554c6017d7cf4549d4dffcd8",
+    "url": "/portfolio/static/media/CV_and_Research_Internship_Paper.e9f1b88c.zip"
+  },
+  {
+    "revision": "7d7ec934906a53e7348c976c3dfde812",
+    "url": "/portfolio/static/media/CanadaImage.7d7ec934.png"
+  },
+  {
+    "revision": "d3c1ed91f40fcd324a68a0b8a56c9215",
+    "url": "/portfolio/static/media/Cluster.d3c1ed91.png"
+  },
+  {
+    "revision": "d074cda20e2b29d649429fc5a555d839",
+    "url": "/portfolio/static/media/Titulo.d074cda2.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ec08dcf1768df5b35746eb9129ce4a6c.js"
+  "/portfolio/precache-manifest.eee901f261689dee78c498c8fbfff994.js"
 );
 
 self.addEventListener('message', (event) => {
