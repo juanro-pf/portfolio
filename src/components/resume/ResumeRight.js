7a8e058d038b4ae014b7b0ddea63d9f407cfc3ba
Juan Ro <juanro.pf@gmail.com>
2022-06-01 14:52:24 -0500

    Skills updated

--- src/components/resume/ResumeRight.js
+++ src/components/resume/ResumeRight.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Certificates from './Certificates';
 import Education from './Education';
 import Experience from './Experience';
-import Skills from './Skills';
+// import Skills from './Skills';
 import portfolioInfo from '../../media/portfolio.json';
 
 const ResumeRight = () => {
   return (
     <div className='resume__container__right'>
-      <Education props={portfolioInfo.resume.education} />
-      <hr className='resume__container__right__separator'/>
       <Experience props={portfolioInfo.resume.experience} />
       <hr className='resume__container__right__separator'/>
-      <Skills props={portfolioInfo.resume.skills} />
+      <Education props={portfolioInfo.resume.education} />  
+      {/* <hr className='resume__container__right__separator'/>
+      <Skills props={portfolioInfo.resume.skills} /> */}
       <hr className='resume__container__right__separator'/>
       <Certificates props={portfolioInfo.resume.certificates} />
     </div>
